e8f271f69b4fbce716a3e8c3fddee09e78203012
Jói Sigurdsson <joi@joisig.com>
2014-05-31 21:48:34 +0000

    Add general file serving.

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var http = require('http');
 var fs = require('fs');
 var vm = require('vm');
 var url = require('url');
+var path = require('path');
 var gufudb = require('./gufudb');
 
 global.lib = '/usr/local/lib/openjscad/';             // for now hard-coded
@@ -17,6 +18,8 @@ var prototype = fs.readFileSync('./prototype.jscad', 'utf-8').toString('utf-8');
 var scad = fs.readFileSync(lib+'./openscad.js');
 var formHtml = fs.readFileSync('./form.html', 'utf-8').toString('utf-8');
 var createdHtml = fs.readFileSync('./created.html', 'utf-8').toString('utf-8');
+
+var otherFiles = {};
 
 function makeModel(params, req, res) {
     res.writeHead(200, {
@@ -57,17 +60,35 @@ function showForm(params, req, res) {
 
 http.createServer(function (req, res) {
     var params = url.parse(req.url, true);
-    var path = params.pathname.toLowerCase();
-    if (path == '/makemodel') {
-	makeModel(params, req, res);
-    } else if (path == '/showform' || path == '/') {
-	showForm(params, req, res);
-    } else if (path == '/makecode') {
-	makeCode(params, req, res);
-    } else {
-	var code = params.pathname.substr(1);
-	console.log("Code requested: " + code);
-	getCode(code, req, res);
-    }
+    var uri = params.pathname, filename = path.join(process.cwd(), uri);
+    var urilower = uri.toLowerCase();
+    path.exists(filename, function(exists) {
+	if (exists && !fs.statSync(filename).isDirectory()) {
+	    fs.readFile(filename, "binary", function(err, file) {
+		if(err) {     
+		    res.writeHead(500, {"Content-Type": "text/plain"});
+		    res.write(err + "\n");
+		    res.end();
+		    return;
+		}
+		
+		res.writeHead(200);
+		res.write(file, "binary");
+		res.end();
+	    });
+	} else {
+	    if (urilower == '/makemodel') {
+		makeModel(params, req, res);
+	    } else if (urilower == '/showform' || urilower == '/') {
+		showForm(params, req, res);
+	    } else if (urilower == '/makecode') {
+		makeCode(params, req, res);
+	    } else {
+		var code = params.pathname.substr(1);
+		console.log("Code requested: " + code);
+		getCode(code, req, res);
+	    }
+	}
+    });
 }).listen(8000, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:8000/');

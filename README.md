9cd095c105a6da96f8d6f6bdc937ecda89649281
Jói Sigurdsson <joi@joisig.com>
2014-05-31 17:13:38 +0000

    Make server entrypoint more generic.

--- README.md
+++ README.md
@@ -5,6 +5,9 @@ gufurnar
 
 ## Getting Started
 
+Note: These instructions have only been verified on a Mac (please
+update if/when verified on other OSes).
+
 1. Clone our repository into some folder.
 2. Go to http://nodejs.org/download/ and install Node.js version 0.8.1 or
 newer.
@@ -15,5 +18,14 @@ newer.
 % git clone https://github.com/Spiritdude/OpenJSCAD.org
 % cd OpenJSCAD.org
 % make install
+% npm install nedb --save
 ```
-a folder that is a subfolder 
+
+## Architecture
+
+The file prototype.jscad is a model containing a dummy string that
+gets embedded in or on the model in an appropriate place. The server
+in make_model.js will take a code=ABBA parameter and the string
+provided will be replaced in prototype.jscad and then OpenJSCAD is
+used to output an STL model of our thing with the provided code
+(e.g. ABBA) embedded in the model.
--- make_model.js
+++ make_model.js
@@ -13,19 +13,30 @@ var CSG = require(lib+'./csg.js').CSG;
 var CAG = require(lib+'./csg.js').CAG;
 
 var prototype = fs.readFileSync('./prototype.jscad', 'utf-8').toString('utf-8');
-console.log(prototype);
-
 var scad = fs.readFileSync(lib+'./openscad.js');
 
-http.createServer(function (req, res) {
+function makeModel(params, req, res) {
     res.writeHead(200, {
 	'Content-Type': 'application/sla',
 	'Content-Disposition': 'attachment; filename="gift-token.stl"'
     });
-    params = url.parse(req.url, true);
     code = params.query.code;
   
     var csg = eval(prototype.replace('42AQLUE', code) + '\n' + scad + '\nmain()');
     res.end(csg.toStlString());
+}
+
+http.createServer(function (req, res) {
+    var params = url.parse(req.url, true);
+    var path = params.pathname.toLowerCase();
+    if (path == '/makemodel') {
+	makeModel(params, req, res);
+    } else if (path == '/showform') {
+    } else if (path == '/makecode') {
+    } else if (path == '/getcode') {
+    } else {
+	res.writeHead(400);
+	res.end("Page not found.");
+    }
 }).listen(8000, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:8000/');
